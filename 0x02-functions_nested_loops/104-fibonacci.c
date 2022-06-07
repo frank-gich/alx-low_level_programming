d0e8703c967d874be469baa9e56daa1b3b99e69e
frank-gich <104795834+frank-gich@users.noreply.github.com>
2022-06-08 00:43:37 +0300

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -  print the first 100 fibonacci numbers, then newline
  *
- * Return: Always 0 (Success)
+ * Return: (0) Success
  */
 
 int main(void)
 {
 	int i;
-	long int fibonacci[50], sum = 2;
 
-	fibonacci[0] = 1;
-	fibonacci[1] = 2;
+	unsigned long int x, y, z;
+	x = 1;
+	y = 2;
 
-	for (i = 2; i < 50; i++)
+	printf("%lu, %lu", x, y);
+	for (i = 0; i < 96; i++)
 	{
-		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
-			sum += fibonacci[i];
+		z = x + y;
+		printf(", %lu", z);
+		x = y;
+		y = z;
 	}
-	printf("%ld\n", sum);
 
+
+	putchar('\n');
 	return (0);
 }
