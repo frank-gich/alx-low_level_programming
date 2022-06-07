c77d50866a7dddb851c19d4c6bfcc5a41b7f2a6e
frank-gich <104795834+frank-gich@users.noreply.github.com>
2022-06-07 20:28:30 +0300

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include main "main.h"
+#include "main.h"
 /**
  * main - prints _putchar, followed by a new line.
  * 
@@ -16,6 +16,6 @@ int main(void)
     _putchar("a");
     _putchar("r");
     _putchar("\n");
-    
+
     return (0)
 }
