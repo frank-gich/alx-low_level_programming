f7ae56d0d4141325ea247de1cd26f4b6478ed124
frank-gich <104795834+frank-gich@users.noreply.github.com>
2022-06-08 23:44:00 +0300

    .

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-positive_or_negative();
-int largest_number();
-print_remaining_days();
-
-  
+#include <stdio.h>
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
