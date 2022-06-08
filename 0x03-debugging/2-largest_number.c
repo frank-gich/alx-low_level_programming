1b8dd9236aeabb28f9882ad50b716887d693b921
frank-gich <104795834+frank-gich@users.noreply.github.com>
2022-06-08 23:49:01 +0300

    .

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,27 +1,37 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- *largest_number - return the largest of threenumbers
- *@a: first integer
- *@b: second integer
- *@c: third integer
- *return: largest number
- */
-int largest_number(int a, int b, int c)
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
+
+void print_remaining_days(int month, int day, int year)
 {
-  int largest;
+	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
+	{
+		if (month > 2 && day >= 60)
+		{
+			day++;
+		}
 
-  if(a >= a && a >= c)
-    {
-      largest = a;
-    }
-  else if (b >= a && b >= c)
-    {
-      largest = b;
-    }
-  else if (c >= a && c >= b)
-    {
-      largest = c;
-    }
-
-  return (largest);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		if (month == 2 && day == 60)
+		{
+			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+		}
+		else
+		{
+			printf("Day of the year: %d\n", day);
+			printf("Remaining days: %d\n", 365 - day);
+		}
+	}
 }
