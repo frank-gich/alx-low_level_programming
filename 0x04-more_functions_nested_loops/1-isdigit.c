a1d70a95b14834e209196d46ab509964db48eb05
frank-gich <104795834+frank-gich@users.noreply.github.com>
2022-06-09 22:54:59 +0300

    .

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
- * _isdigit - checks for characters that are digits
- * @c: character 
+ * _isupper - uppercase letters
+ * @c: character to check
  *
  * Return: 0 or 1
  */
 
 
-int _isdigit(int c)
+int _isupper(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
