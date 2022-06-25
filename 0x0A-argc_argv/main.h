d2161122342e590669ece348a7a8237c660ee47d
frank-gich <104795834+frank-gich@users.noreply.github.com>
2022-06-25 22:25:04 +0300

    .

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,6 +6,7 @@ int main(int argc, __attribute__((unused)) char *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 
 #endif /* MAIN_H */
