aa6c2a10d0f378cd72884448ecf8d8993cf6e7d8
frank-gich <gichuhi5781@gmail.com>
2023-02-11 03:00:50 +0300

    changes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,27 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_create - creates a hash tables
- * @size: the array (hash table) size
- * Return: a  pointer to the newly created hash table
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
+ *
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ptr_to_array;
+	hash_table_t *ht;
+	unsigned long int i;
 
-	if (size == 0)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
 
-	ptr_to_array = calloc(1, sizeof(hash_table_t));
-	if (ptr_to_array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
-	ptr_to_array->size = size;
-	/* The  calloc()  function allocates memory for an array */
-	/* void *calloc(size_t nmemb, size_t size) */
-	ptr_to_array->array = calloc(size, sizeof(hash_table_t **));
-	if (ptr_to_array->array == NULL)
-	{
-		free(ptr_to_array);
-		return (NULL);
-	}
-	return (ptr_to_array);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
 }
